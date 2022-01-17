a1beb4147f1ba5d01046712477f2a719feaccd46
trevin-livele <liveletrevin6@gmail.com>
2022-01-17 08:34:39 +0300

    added page-display route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './header/header.component';
+import { PageDisplayComponent } from './page-display/page-display.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about', component: AboutUsComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: 'Header', component: HeaderComponent},
+  { path:  'page-display', component: PageDisplayComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
