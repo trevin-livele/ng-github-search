9273fbd8786e37c607209505829f6343f7ed6d3b
trevin-livele <liveletrevin6@gmail.com>
2022-01-17 08:47:10 +0300

    added page not found

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './header/header.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactComponent } from './contact/contact.component';
 import { PageDisplayComponent } from './page-display/page-display.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { PageDisplayComponent } from './page-display/page-display.component';
     HeaderComponent,
     AboutUsComponent,
     ContactComponent,
-    PageDisplayComponent
+    PageDisplayComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
