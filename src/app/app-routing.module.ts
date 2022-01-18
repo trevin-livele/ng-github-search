9fda9d4e66bd7bd52bb7fa7cf161e4fdb9508e3b
trevin-livele <liveletrevin6@gmail.com>
2022-01-18 11:29:35 +0300

    edited routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactComponent } from './contact/contact.component';
 import { HeaderComponent } from './header/header.component';
-import { PageDisplayComponent } from './page-display/page-display.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   { path: 'about', component: AboutUsComponent},
   { path: 'contact', component: ContactComponent},
   { path: 'Header', component: HeaderComponent},
-  { path:  'page-display', component: PageDisplayComponent },
   { path:  '**', component:NotFoundComponent },
   { path: '', redirectTo:"/goals", pathMatch:"full" },
 ];
